0e4f9d71bda4a71a4ed4fbd5e93935a459dac829
ebarsula <ebarsulai@gmail.com>
2025-06-13 02:50:48 +0300

    feat: add show action to CartsController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
   before_action :set_product, only: [:add_item, :remove_item]
 
+  def show
+    @cart_items = current_cart.cart_items.includes(:product)
+  end
+
   private
 
   def set_product
