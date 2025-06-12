83e5ec0a0c94ea98106b299f441b32116b46a250
ebarsula <ebarsulai@gmail.com>
2025-06-13 00:09:57 +0300

    update: Add many to many relationship to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
  
   before_destroy :not_referenced_by_any_line_item
   belongs_to :user, optional: true
+  has_and_belongs_to_many :carts
 
   mount_uploader :image, ImageUploader
   serialize :image, JSON # If you use SQLite, add this line
