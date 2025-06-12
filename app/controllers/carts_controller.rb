6440ff80a4e3af607ee10c3ee15dde8c1b4250ee
ebarsula <ebarsulai@gmail.com>
2025-06-13 02:46:10 +0300

    feat: add CartsController with basic cart management actions

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,14 @@
+class CartsController < ApplicationController
+    before_action :set_product, only: [:add_item, :remove_item]
+  def show
+  end
+
+  def add_item
+  end
+
+  def remove_item
+  end
+
+  def empty
+  end
+end
