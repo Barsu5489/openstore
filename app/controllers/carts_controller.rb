45b86abeb88a7097e4b0f33854465dec4e93c0d9
ebarsula <ebarsulai@gmail.com>
2025-06-13 02:50:48 +0300

    feat: add add_item action to CartsController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,6 +5,14 @@ class CartsController < ApplicationController
     @cart_items = current_cart.cart_items.includes(:product)
   end
 
+  def add_item
+    @cart_item = current_cart.cart_items.find_or_initialize_by(product: @product)
+    @cart_item.quantity += 1
+    @cart_item.save
+    flash[:notice] = "Added to your cart"
+    redirect_to cart_path
+  end
+
   private
 
   def set_product
