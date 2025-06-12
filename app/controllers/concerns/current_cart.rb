ee0af6ca4018616e089e5711988a1c30bf8b8859
ebarsula <ebarsulai@gmail.com>
2025-06-13 02:45:09 +0300

    Update current_cart to use find_by and ensure new cart creation when session cart is missing

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -8,13 +8,16 @@ end
 
   private
 
-  def current_cart
-    if user_signed_in?
-      @current_cart ||= current_user.cart || current_user.create_cart
-    else
-      @current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
-      @current_cart ||= Cart.create
+def current_cart
+  if user_signed_in?
+    @current_cart ||= current_user.cart || current_user.create_cart
+  else
+    @current_cart ||= Cart.find_by(id: session[:cart_id])
+    unless @current_cart
+      @current_cart = Cart.create
       session[:cart_id] = @current_cart.id
     end
+    @current_cart
   end
+end
 end
