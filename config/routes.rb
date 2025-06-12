ad26388baf8cdde21f003e9b8dd792937e8e8728
ebarsula <ebarsulai@gmail.com>
2025-06-13 02:59:34 +0300

    feat: add cart resource routes with add_item, remove_item, and empty actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
   resources :products
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+  resource :cart, only: [:show] do
+  post 'add_item/:product_id', to: 'carts#add_item', as: 'add_item'
+  delete 'remove_item/:product_id', to: 'carts#remove_item', as: 'remove_item'
+  delete 'empty', to: 'carts#empty', as: 'empty'
+end
+
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
