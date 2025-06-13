581af7d051df484720e32a10bd6dab8d3181c3eb
ebarsula <ebarsulai@gmail.com>
2025-06-13 04:34:55 +0300

    feat: Add increase and decrease routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post 'add_item/:product_id', to: 'carts#add_item', as: 'add_item'
   delete 'remove_item/:product_id', to: 'carts#remove_item', as: 'remove_item'
   delete 'empty', to: 'carts#empty', as: 'empty'
+  post 'increase_item/:product_id', to: 'carts#increase_item', as: 'increase_item'
+  post 'decrease_item/:product_id', to: 'carts#decrease_item', as: 'decrease_item'
 end
 
   root 'products#index'
